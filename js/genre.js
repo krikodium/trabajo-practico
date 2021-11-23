4eed9581431e22d6b5097394a4bceea003c9ccea
Juan Pagola <juanpagola@mbp-de-pagola-2.lan>
2021-11-23 13:43:29 -0300

    agrego generos y populares

---
+++ js/genre.js
@@ -0,0 +1,54 @@
+let urlPeli = 'https://api.themoviedb.org/3/genre/movie/list?api_key=eff71d911bc68b6cdf67ad2d02630320&language=en-US';
+
+fetch(urlPeli)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data)
+        let contenedor = document.querySelector('.contentGenre')
+        let generos = data.genres;
+        let losGeneros = '';
+        console.log(generos)
+        
+        for(let i = 0; i < generos.lenght; i++){
+            losGeneros += `
+                <div class="generos">
+                    <a href="detail-genre.html?id=${generos[i].id}&title=${generos[i].name}"><p class="genreTitle">${generos[i].name}</p></a>
+                </div>
+            `
+        }
+        contenedor.innerHTML = losGeneros;
+    })
+    .catch(function (error) {
+        console.log('este es el error = ' + error)
+    })
+
+    //generos de series
+
+    let urlSerie =`https://api.themoviedb.org/3/genre/tv/list?api_key=eff71d911bc68b6cdf67ad2d02630320&language=en-US`;
+
+    fetch(urlSerie)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+        let contenedor = document.querySelector('.contentGenreS')
+        let generos = data.genres;
+        console.log(generos);
+        let losGeneros = ``;
+
+        for(let i=0; i<generos.lenght; i++){
+            losGeneros += `
+            <div class="generosS">
+                <a href="detail-genreSeries.html?id=${generos[i].id}&title=${generos[i].name}"><p class="genreTitle">${generos[i].name}</p></a>
+            </div>
+            `
+        }
+        contenedor.innerHTML= losGeneros;
+    })
+    .catch(function(error){
+        console.log(`Este es el error = ` + error);
+    })
+
